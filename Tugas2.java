8380da70cb0ded2962ee82b39c3a9dfdbc350a68
dianakholida <43032603+dianakholida@users.noreply.github.com>
2018-09-06 21:37:28 +0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author thosiba
+ */
+public class Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 5;
+        for (int i=1;i<=jumlah;i++){
+            for (int x=1;x<=i;x++){
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+    
+}
